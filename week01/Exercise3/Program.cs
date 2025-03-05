27e509125f1be593c092b1bf69bde4eaeeaf3cd1
codejoeylee <benjaminoforiasante@gmail.com>
2025-03-05 09:54:53 +0000

    Exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        int yourGuess = 0;
+        int randomNumber = 0;
+        List<int> myList = new List<int>();
+        string play = "yes";
+        while (play == "yes")
+        {
+            do
+            {
+                Random random = new Random();
+                randomNumber = random.Next(0, 3);
+                Console.Write("What is your guess? ");
+                yourGuess = Convert.ToInt32(Console.ReadLine());
+                myList.Add(yourGuess);
+
+                if (yourGuess == randomNumber)
+                {
+                    Console.WriteLine("you guessed it!");
+                }
+                else if (yourGuess < randomNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("Lower");
+                }
+
+            } while (yourGuess != randomNumber);
+
+            Console.WriteLine("Your Guesses : ");
+            foreach (int Guess in myList)
+            {
+                Console.WriteLine(Guess);
+            }
+
+            Console.WriteLine("Do you want to play again: ");
+            play = Convert.ToString(Console.ReadLine());
+        }
     }
 }
